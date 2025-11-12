dac8211ead6d1754d12e7db525eafd13c2eae27e
Spencer Gable-Cook <spencer.gable-cook@uhn.ca>
2025-11-12 00:18:38 -0500

    Displaying users to choose from

--- ChatSean/source/app.tsx
+++ ChatSean/source/app.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text } from 'ink';
+import 'dotenv/config';
+import SelectInput from 'ink-select-input';
 
 type Props = {
 	username?: string;
@@ -13,10 +15,16 @@ type User = {
 	email: string
 };
 
-export default function App({ username, password }: User) {
+export default function App({ username, password }: Props) {
+	// UseState for users and logged in
+	const [users, setUsers] = useState<User[]>([]);
+	const [loggedIn, setLoggedIn] = useState<boolean>(true);
+	const [error, setError] = useState<string | null>(null);
+
+	const BASE_URL = process.env['BACKEND_URL'];
+
 	// POST request to /login/ with the username and password
-	// Check if user is successfully signed in
-	let loggedIn: boolean = false;
+
 	// If not, we will show them a message
 	if (!loggedIn) {
 		return (
@@ -27,11 +35,40 @@ export default function App({ username, password }: User) {
 	}
 
 	// GET request to /users/ to get all users
-	let all_users: User[];
+	// let all_users: User[];
+	useEffect(() => {
+		if (loggedIn) {
+			fetch(`${BASE_URL}/users`)
+				.then(res => res.json())
+				.then((data: User[]) => setUsers(data))
+				.then(() => setLoggedIn(true))
+				.catch(err => setError(err.message));
+		}
+	}, [loggedIn]);
+
+	if (error) {
+		return <Text color="red">Oops! {error}</Text>;
+	}
+
+	// Convert users into SelectInput items
+	const items = users.map(user => ({
+		label: `${user.name} (${user.username})`,
+		value: user,
+		key: user.username
+	}));
 
 	return (
-		<Text>
-			Hello, <Text color="green">{username}/{password}</Text>
-		</Text>
+		<>
+			<Text>
+				Hello, <Text color="green">{username}/{password}</Text>
+			</Text>
+			<SelectInput
+				items={items}
+				onSelect={item => {
+					const user = item.value as User;
+					console.log('Selected user:', user);
+				}}
+			/>
+		</>
 	);
 }
